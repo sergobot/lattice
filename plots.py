dfed1a89d3d931887254588f59bc2a76d557d501
Sergey Popov <me@sergobot.me>
2019-12-25 04:22:24 +0300

    Update plots.py

--- plots.py
+++ plots.py
@@ -2,16 +2,22 @@
 # coding: utf-8
 
 import pandas as pd
+import numpy as np
+import seaborn as sns
 from matplotlib import pyplot as plt
 
 df = pd.read_csv('out/percolation_thresholds.csv')
-grouped_expanding = df.groupby('size').apply(lambda s: s.reset_index())['threshold'].expanding(min_periods=1)
-expanding_mean = grouped_expanding.mean().unstack().T
-expanding_std = grouped_expanding.std().unstack().T
+grouped_by_size = df.groupby('size')
+
+size_mean = grouped_by_size.apply(np.mean)
+size_std = grouped_by_size.apply(np.std)
+size_std['size'] = size_std.index
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
-expanding_mean.plot(ax=ax1, grid=True, logx=True, title='Mean')
-expanding_std.plot(ax=ax2, grid=True, logx=True, title='Standard deviation')
+ax1.set_title('Mean threshold value')
+ax2.set_title('Standard deviation')
+sns.barplot('size', 'threshold', data=size_mean, ax=ax1)
+sns.barplot('size', 'threshold', data=size_std, ax=ax2)
 
 plt.savefig('plots.png')
 plt.show()
